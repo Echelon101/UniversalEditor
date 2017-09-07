ea8d42d57d6e03707486b4c916c1e24c81ff58ba
Echelon101 <timomrx1@gmail.com>
2017-09-07 16:57:43 +0200

    Split some data to make it esier

---
+++ ApiClient/ApiClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiClient
+{
+    class ApiClient : HttpClient
+    {
+        public static HttpClient client = new HttpClient();
+        public void Client()
+        {
+            var byteArray = Encoding.ASCII.GetBytes("demo:RCiG7xCeTwHm0IFnNDXtwi2OFZPR5tMmv2PqJi9q");
+            client.BaseAddress = new Uri("http://shop.localtest.me/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+        }
+    }
+}
---
+++ ApiClient/Ressource.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiClient
+{
+    public class Article
+    {
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        public int Tax { get; set; }
+
+    }
+    public class Customer
+    {
+
+    }
+    public class Order
+    {
+
+    }
+    public class Translation
+    {
+
+    }
+    public class Category
+    {
+
+    }
+    public class Media
+    {
+
+    }
+}
